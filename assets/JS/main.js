1aa2cc521f8d085e66c4774d4a569f27d133e310
George Small <gsrr201@gmail.com>
2026-07-15 20:02:19 +0100

    changed .nav-menu to .nav-toggle-label in JavaScript Code

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav-toggle-label");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
