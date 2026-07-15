fdbac11affd9b7a0ae504c85bd42c127eab6218e
George Small <gsrr201@gmail.com>
2026-07-15 19:57:01 +0100

    started to add JavaScript code for nav bar

---
+++ assets/JS/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Small Scale Studios loaded");
+
+});
+
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
