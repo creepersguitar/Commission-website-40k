0a71106775aaea770c8f732f4c071ba1f1c5e08a
George Small <gsrr201@gmail.com>
2026-07-15 20:47:37 +0100

    tidied code for each file to test hamburger issue

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,12 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("Small Scale Studios loaded");
 
-});
+    const navToggle = document.querySelector(".nav-toggle");
+    const navMenu = document.querySelector(".nav-menu");
 
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
 
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
 });
