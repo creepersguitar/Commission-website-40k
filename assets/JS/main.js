45ba02dc094101e65e58454518a2be86bf7e70b2
George Small <gsrr201@gmail.com>
2026-07-15 21:09:04 +0100

    changed JS code to sort out hamburger issue

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.querySelector(".nav-toggle");
     const navMenu = document.querySelector(".nav-menu");
 
+    console.log(navToggle);
+    console.log(navMenu);
+
     navToggle.addEventListener("click", () => {
         navMenu.classList.toggle("active");
     });
